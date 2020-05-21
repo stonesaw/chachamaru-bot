6a148b9961a70ddb5ad60e9ffae85f3cadca2e25
stonesaw <ishichan1260@gmail.com>
2020-05-21 21:03:06 +0900

    Add lib/

--- app.py
+++ app.py
@@ -13,9 +13,27 @@ load_dotenv(verbose=True)
 
 client = discord.Client()
 
-json_open = open('data.json', mode='r', encoding='utf-8')
+json_open = open('lib/data.json', mode='r', encoding='utf-8')
 json_data = json.load(json_open)
 json_open.close
+
+global js
+js = {
+    "greeting":       list(json_data['greeting'].values()),
+    "call":           list(json_data['call'].values()),
+    "call_keywords":  list(json_data['call_keywords'].values()),
+    "grass_keywords": list(json_data['grass_keywords'].values()),
+    "face":           list(json_data['face'].values()),
+    "cm":             list(json_data['cm'].values()),
+    "song":           list(json_data['song'].values()),
+    "help_all":             "\n".join(list(json_data['help']['all'].values())),
+    "help_funfun":          "\n".join(list(json_data['help']['funfun'].values())),
+    "help_funfun_time_set": "\n".join(list(json_data['help']['funfun_time_set'].values()))
+}
+
+global time_call
+time_call = json_data['time_call']
+
 
 @client.event
 async def on_ready():
@@ -23,25 +41,6 @@ async def on_ready():
     # global voice_channel
     send_channel = client.get_channel(712237427460407316)
     # voice_channel = client.get_channel(683939861539192865)
-
-    global greeting
-    global call
-    global face
-    global cm
-    global song
-    global help_all
-    global help_funfun
-    global help_funfun_time_set
-    global time_call
-    greeting = list(json_data['greeting'].values())
-    call =     list(json_data['call'].values())
-    face =     list(json_data['face'].values())
-    cm =       list(json_data['cm'].values())
-    song =     list(json_data['song'].values())
-    help_all =    "\n".join(list(json_data['help']['all'].values()))
-    help_funfun = "\n".join(list(json_data['help']['funfun'].values()))
-    help_funfun_time_set = "\n".join(list(json_data['help']['funfun_time_set'].values()))
-    time_call = json_data['time_call']
     
     global funfun_done
     funfun_done = True
@@ -56,39 +55,44 @@ async def on_message(message):
         return
 
     # チャンネル: ちゃちゃのまる部屋のみ
-    # if message.channel.id != send_channel:
-    #     return
+    if message.channel != send_channel:
+        return
 
     gets = message.content
 
     if gets == 'ちゃちゃまる':
         # TO DO
-        greeting[0] = f'へー、おまえ{message.author.display_name}っていうのか！\nなんだかおまえとは、仲良くできる気がするぞ！ふんふん！'
-        await message.channel.send(random.choice(greeting))
-    else: 
-        for i in ['ちゃ', 'まる', '筋肉', '筋トレ', 'ひつじ', '羊']:
+        js['greeting'][0] = f'へー、おまえ{message.author.display_name}っていうのか！\nなんだかおまえとは、仲良くできる気がするぞ！ふんふん！'
+        await message.channel.send(random.choice(js['greeting']))
+    else:
+        for i in js['call_keywords']:
             if i in gets:
-                await message.channel.send(random.choice(call))
+                await message.channel.send(random.choice(js['call']))
                 return
+
+    for i in js['grass_keywords']:
+        if i in gets:
+            await message.channel.send(file=discord.File('lib/grass.png'))
+            return
 
     # All ChaCha's Greeting
     # if gets.startswith('!greet'):
-    #     await message.channel.send('\n'.join(greeting))
+    #     await message.channel.send('\n'.join(js['greeting']))
     
     if gets.startswith('!face'):
-        await message.channel.send(random.choice(face))
+        await message.channel.send(random.choice(js['face']))
         
     if gets.startswith('!cm'):
         await message.channel.send('もって来たぞー')
-        await message.channel.send(random.choice(cm))
+        await message.channel.send(random.choice(js['cm']))
 
     if gets.startswith('!song'):
         await message.channel.send('もって来たぞー')
-        await message.channel.send(random.choice(song))
+        await message.channel.send(random.choice(js['song']))
 
     # show all help
     if gets.startswith('!chacha'):
-        await message.channel.send(help_all)
+        await message.channel.send(js['help_all'])
 
     if re.match(r'^!funfun(\s+)true', gets.lower()):
         funfun_done = True
@@ -99,9 +103,9 @@ async def on_message(message):
     elif gets == '!funfun':
         await message.channel.send(str(funfun_done))
     elif re.match(r'^!funfun(\s)help', gets):
-        await message.channel.send(help_funfun)
+        await message.channel.send(js['help_funfun'])
     elif re.match(r'^!funfun(\s)time_set', gets):
-        await message.channel.send(help_funfun_time_set)
+        await message.channel.send(js['help_funfun_time_set'])
     # TO DO
     # add, del, list
 
@@ -109,7 +113,7 @@ async def on_message(message):
 async def every():
     time = datetime.datetime.now().time()
     if funfun_done and time.minute == 0:
-        print('time call')
+        print('time called')
         # 6 - 8 getup
         if time.hour in range(6, 8):
             await send_channel.send(time_call['getup']['kintore'])
